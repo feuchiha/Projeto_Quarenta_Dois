a8b4c4a14e863dbb8c253ae7c433b0ea9f5ac67b
fevalim <felipecgvalim@gmail.com>
2019-04-11 20:22:50 -0300

    Update customJS.js

--- ProjetoQD/src/assets/jscustom/customJS.js
+++ ProjetoQD/src/assets/jscustom/customJS.js
@@ -1,22 +1,22 @@
-// function carregaModal() {
-//     document.querySelector('.bg-modal').style.display = "flex";
-// }
+// // function carregaModal() {
+// //     document.querySelector('.bg-modal').style.display = "flex";
+// // }
 
-// function fechaModal() {
-//     document.querySelector('.bg-modal').style.display = "none";
-// }
+// // function fechaModal() {
+// //     document.querySelector('.bg-modal').style.display = "none";
+// // }
 
-// function(){
-//     var modal = document.getElementById("ButtonImportURL");
-//     modal.addEventListener("click", carregaModal);
-//     var fechaM = document.getElementById("close");
-//     fechaM.addEventListener("click", fechaModal);
-// }
+// // function(){
+// //     var modal = document.getElementById("ButtonImportURL");
+// //     modal.addEventListener("click", carregaModal);
+// //     var fechaM = document.getElementById("close");
+// //     fechaM.addEventListener("click", fechaModal);
+// // }
 
-document.getElementById('ButtonImportURL').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "flex";
-});
+// document.getElementById('ButtonImportURL').addEventListener("click", function() {
+// 	document.querySelector('.bg-modal').style.display = "flex";
+// });
 
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.bg-modal').style.display = "none";
-});
+// document.querySelector('.close').addEventListener("click", function() {
+// 	document.querySelector('.bg-modal').style.display = "none";
+// });
