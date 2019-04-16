c13f858221c83ef131c545901209d9ce0cdf1550
fevalim <felipecgvalim@gmail.com>
2019-04-15 21:20:37 -0300

    App Component Toastr

--- ProjetoQD/src/app/app.component.ts
+++ ProjetoQD/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrManager } from 'ng6-toastr-notifications';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  
+  constructor(public toastr: ToastrManager) {
+  }
+ 
+  showSuccess() {
+      this.toastr.successToastr('This is success toast.', 'Success!');
+  }
+
+  showError() {
+      this.toastr.errorToastr('This is error toast.', 'Oops!');
+  }
+
+  showWarning() {
+      this.toastr.warningToastr('This is warning toast.', 'Alert!');
+  }
+
+  showInfo() {
+      this.toastr.infoToastr('This is info toast.', 'Info');
+  }
+
+  showCustom() {
+      this.toastr.customToastr(
+      '<span style="color: green; font-size: 16px; text-align: center;">Custom Toast</span>',
+      null,
+      { enableHTML: true }
+      );
+  }
+
+  showToast(position: any = 'top-left') {
+      this.toastr.infoToastr('This is a toast.', 'Toast', {
+          position: position
+      });
+  }
  }
