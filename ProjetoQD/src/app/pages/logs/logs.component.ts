e701522ec6b95651ee9c6631a118eef89ff3455d
AlvaroAugustoZago <alvaro_zago@estudante.sc.senai.br>
2019-04-22 20:32:09 -0300

    Alteração de titulo
    Alteração de Localhost para Arquivo

--- ProjetoQD/src/app/pages/logs/logs.component.ts
+++ ProjetoQD/src/app/pages/logs/logs.component.ts
@@ -24,10 +24,10 @@ const table: DialogData[] = [
           {titulo:"Unidades Básicas de Saúde em Construção",
    dataInsercao:"28/12/2018",
    inserido:false,
-   lugar:'localhost',
-  campos:["Código Cnes", "Latitude", "Longitude",
-          "Nome Fantasia", "Nome Logradouro", "Número do Endereço", "Nome do Bairro",
-          "Número de Telefone", "Código CEP", "UF", "Cidade"]}
+   lugar:'Arquivo',
+   campos:["Código Cnes", "Latitude", "Longitude",
+           "Nome Fantasia", "Nome Logradouro", "Número do Endereço", "Nome do Bairro",
+           "Número de Telefone", "Código CEP", "UF", "Cidade"]}
   ]
 
 @Component({
