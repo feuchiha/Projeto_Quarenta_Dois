0c10130cfe2acadc42b297fc663e4b0baf09f172
Anderson Paterno <anderson_paterno@estudante.sc.senai.br>
2019-04-23 21:17:07 -0300

    users list

--- ProjetoQD/src/app/pages/users/users.component.ts
+++ ProjetoQD/src/app/pages/users/users.component.ts
@@ -1,7 +1,8 @@
 
 import {Component, OnInit} from '@angular/core';
 import {MatTableDataSource} from '@angular/material';
-import { keys } from 'd3';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ToastrManager } from 'ng6-toastr-notifications';
 
 export interface PeriodicElement {
   user: string;
@@ -10,21 +11,41 @@ export interface PeriodicElement {
   status:string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {user: 'Haruno', created: '2019/04/07', email: 'sakura@haruno.com', status:"INATIVO"},
-  {user: 'Uchiha', created: '2019/04/07', email: 'sasuke@uchiha.com', status:"User"},
-  {user: 'Uzumaki', created: '2019/04/07', email: 'naruto@uzumaki.com', status:"Admin"}
-];
+const ELEMENT_DATA: PeriodicElement[] = [];
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css' ]
 })
-export class UsersComponent  {
+export class UsersComponent implements OnInit {
+  
   displayedColumns: string[]  = ['user', 'status', 'created', 'email','aa'];
- 
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  dataSource: any
+  constructor(private http: HttpClient, public toastr: ToastrManager) {
+  }
 
+  ngOnInit() {
+    const headers = new HttpHeaders()
+          .set('Authorization', 'my-auth-token')
+          .set('Content-Type', 'application/json');
+    this.http.post(`http://localhost:3002/users/listusers`,{
+    headers: headers
+    })
+    .subscribe(data => {
+      for(let obj in data['data']){
+        ELEMENT_DATA.push({
+          user: data['data'][obj]['name'],
+          created: data['data'][obj]['created'],
+          email: data['data'][obj]['email'],
+          status:data['data'][obj]['status']
+        })
+      }
+      this.dataSource = new MatTableDataSource(ELEMENT_DATA);      
+    });
+    
+  }
+
+ 
   applyFilter(filterValue: string) {
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
