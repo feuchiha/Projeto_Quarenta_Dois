c8b381f3eb75174b79f120c8ccab1522392e3903
Anderson Paterno <anderson_paterno@estudante.sc.senai.br>
2019-04-26 19:30:38 -0300

    combo box importação

--- ProjetoQD/src/app/pages/pages/altera-senha/altera-senha.component.ts
+++ ProjetoQD/src/app/pages/pages/altera-senha/altera-senha.component.ts
@@ -2,8 +2,6 @@ import { Component, HostBinding, NgModule, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ToastrManager } from 'ng6-toastr-notifications';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { PARAMETERS } from '@angular/core/src/util/decorators';
-import { ConstantPool } from '@angular/compiler';
 import { BlankLayoutCardComponent } from 'app/components/blank-layout-card';
 
 @Component({
@@ -11,6 +9,7 @@ import { BlankLayoutCardComponent } from 'app/components/blank-layout-card';
   templateUrl: './altera-senha.component.html',
   styleUrls: ['../../../components/blank-layout-card/blank-layout-card.component.scss']
 })
+
 export class AlteraSenhaComponent extends BlankLayoutCardComponent {
   back = {user:{password:""}, params: {token:""}};
    
