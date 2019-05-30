eba09b548627f20a3326ec37d5ce7b6086bfce62
fevalim <felipecgvalim@gmail.com>
2019-05-30 19:02:04 -0300

    CardColumn teste

--- ProjetoQD/src/app/pages/dashboard/cardcolumn/cardcolumn.component.ts
+++ ProjetoQD/src/app/pages/dashboard/cardcolumn/cardcolumn.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-cardcolumn',
   templateUrl: './cardcolumn.component.html',
-  styleUrls: ['./cardcolumn.component.scss']
+  styleUrls: ['./cardcolumn.component.css']
 })
 export class CardcolumnComponent implements OnInit {
 
