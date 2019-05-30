7e97796004ecec2536745ce3589ad7e2a5f97251
fevalim <felipecgvalim@gmail.com>
2019-05-30 18:55:42 -0300

    Cards gerados
    Vide commit feito na branch valim que expliquei sobre os cards.

---
+++ ProjetoQD/src/app/pages/dashboard/cardgeo/cardgeo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardgeo',
+  templateUrl: './cardgeo.component.html',
+  styleUrls: ['./cardgeo.component.scss']
+})
+export class CardgeoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ ProjetoQD/src/app/pages/dashboard/cardpie/cardpie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cardpie',
+  templateUrl: './cardpie.component.html',
+  styleUrls: ['./cardpie.component.scss']
+})
+export class CardpieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
