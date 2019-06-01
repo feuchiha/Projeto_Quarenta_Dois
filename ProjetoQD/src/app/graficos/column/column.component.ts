0e8dea2c322453150a20007bc78657031cb0265d
fevalim <felipecgvalim@gmail.com>
2019-06-01 18:59:52 -0300

    Graficos 2
    Graficos implementados sem css alterado.

--- ProjetoQD/src/app/graficos/column/column.component.ts
+++ ProjetoQD/src/app/graficos/column/column.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+declare var google: any;
 @Component({
   selector: 'app-column',
-  templateUrl: './column.component.html',
+  template: '<div #columnChart></div>',
   styleUrls: ['./column.component.scss']
 })
-export class ColumnComponent implements OnInit {
+export class ColumnComponent implements AfterViewInit {
 
-  constructor() { }
+  @ViewChild('columnChart') columnChart: ElementRef
 
-  ngOnInit() {
+  drawChart = () => {
+    var data = google.visualization.arrayToDataTable([
+      ['Genre', 'Fantasy & Sci Fi', 'Romance', 'Mystery/Crime', 'General',
+       'Western', 'Literature', { role: 'annotation' } ],
+      ['2010', 10, 24, 20, 32, 18, 5, ''],
+      ['2020', 16, 22, 23, 30, 16, 9, ''],
+      ['2030', 28, 19, 29, 30, 12, 13, '']
+    ]);
+    var options = {
+      width: 600,
+      height: 400,
+      legend: { position: 'top', maxLines: 3 },
+      bar: { groupWidth: '75%' },
+      isStacked: true,
+    };
+    const chart = new google.visualization.ColumnChart(this.columnChart.nativeElement);
+    chart.draw(data, options);
   }
 
+  ngAfterViewInit() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(this.drawChart);
+  }
 }
--- ProjetoQD/src/app/graficos/geo/geo.component.ts
+++ ProjetoQD/src/app/graficos/geo/geo.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+declare var google: any;
 
 @Component({
   selector: 'app-geo',
-  templateUrl: './geo.component.html',
+  template: '<div #geoChart></div>',
   styleUrls: ['./geo.component.scss']
 })
-export class GeoComponent implements OnInit {
+export class GeoComponent implements  AfterViewInit{
 
-  constructor() { }
+  @ViewChild('geoChart') geoChart: ElementRef
 
-  ngOnInit() {
+  drawChart = () => {
+    var data = google.visualization.arrayToDataTable([
+      ['City',   'Population', 'Area'],
+      ['Rome',      2761477,    1285.31],
+      ['Milan',     1324110,    181.76],
+      ['Naples',    959574,     117.27],
+      ['Turin',     907563,     130.17],
+      ['Palermo',   655875,     158.9],
+      ['Genoa',     607906,     243.60],
+      ['Bologna',   380181,     140.7],
+      ['Florence',  371282,     102.41],
+      ['Fiumicino', 67370,      213.44],
+      ['Anzio',     52192,      43.43],
+      ['Ciampino',  38262,      11]
+    ]);
+    var options = {
+      region: 'IT',
+      displayMode: 'markers',
+      colorAxis: {colors: ['green', 'blue']}
+    };
+
+    const chart = new google.visualization.GeoChart(this.geoChart.nativeElement);
+    chart.draw(data, options);
+  }
+
+
+  ngAfterViewInit() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(this.drawChart);
   }
 
 }
