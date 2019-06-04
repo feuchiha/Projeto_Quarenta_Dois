a83b141dcd680d1bc0407dfad179824a96276bc1
Paterninho <anderson_paterno@estudante.sc.senai.br>
2019-06-04 18:17:13 -0300

    Revert "Commit SQL"
    This reverts commit aa1e46f06c04bc89e1e599f36ac36fba5258e491.

--- ProjetoQD/src/app/graficos/line/line.component.ts
+++ ProjetoQD/src/app/graficos/line/line.component.ts
@@ -1,48 +1,24 @@
-import { Component, ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ToastrManager } from 'ng6-toastr-notifications';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 declare var google: any;
-
 @Component({
   selector: 'app-line',
   template: '<div #lineChart></div>',
   styleUrls: ['./line.component.scss']
 })
-export class LineComponent implements OnInit {
-
-  arrData:any = [];
-  arrCab: any = [];
-  arrValues: any = [];
+export class LineComponent implements AfterViewInit {
 
   @ViewChild('lineChart') lineChart: ElementRef
 
-
-  constructor(private http: HttpClient, public toastr: ToastrManager) {
-  }
-
-  ngOnInit(): void {
-    google.charts.load('current', { 'packages': ['corechart'] });
-    const headers = new HttpHeaders()
-    .set('Authorization', 'my-auth-token')
-    .set('Content-Type', 'application/json')
-    this.http.post(`http://localhost:3002/Mysql/line`,{
-      headers: headers
-    })
-    .subscribe(data => {
-      for (const k in data) {
-          const element = data[k];
-          this.arrCab.push( element['Regio']);
-          this.arrValues.push([parseInt(element['M']), parseInt(element['F'])]);
-      }
-      this.arrData.push(this.arrCab);
-      this.arrData.push(this.arrValues);
-      console.log(this.arrData)
-      google.charts.setOnLoadCallback(this.drawChart);
-    })
-  }
-
   drawChart = () => {
-    var data = google.visualization.arrayToDataTable(this.arrData);
+
+    var data = google.visualization.arrayToDataTable([
+      ['Year', 'Sales', 'Expenses'],
+      ['2004',  1000,      400],
+      ['2005',  1170,      460],
+      ['2006',  660,       1120],
+      ['2007',  1030,      540]
+    ]);
+
     const options = {
       title: 'Company Performance',
       curveType: 'function',
@@ -52,6 +28,11 @@ export class LineComponent implements OnInit {
 
     const chart = new google.visualization.LineChart(this.lineChart.nativeElement);
     chart.draw(data, options);
+  }
+
+  ngAfterViewInit() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(this.drawChart);
   }
 
 }
--- ProjetoQD/src/app/graficos/pie/pie.component.ts
+++ ProjetoQD/src/app/graficos/pie/pie.component.ts
@@ -1,6 +1,4 @@
-import { Component, ViewChild, ElementRef, AfterViewInit, OnInit } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { ToastrManager } from 'ng6-toastr-notifications';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 declare var google: any;
 
 @Component({
@@ -8,40 +6,19 @@ declare var google: any;
   template: '<div #pieChart></div>',
   styleUrls: ['./pie.component.css']
 })
-export class PieComponent implements OnInit { 
-  arrData:any = [];
-  arrCab: any = [];
-  arrValues: any = [];
-
+export class PieComponent implements AfterViewInit { 
   @ViewChild('pieChart') pieChart: ElementRef
-
-  constructor(private http: HttpClient, public toastr: ToastrManager) {
-  }
-
-  ngOnInit(): void {
-    google.charts.load('current', { 'packages': ['corechart'] });
-    const headers = new HttpHeaders()
-    .set('Authorization', 'my-auth-token')
-    .set('Content-Type', 'application/json')
-    this.http.post(`http://localhost:3002/Mysql/pie`,{
-      headers: headers
-    })
-    .subscribe(data => {
-      for (const k in data) {
-          const element = data[k];
-          this.arrCab.push( element['Regio']);
-          this.arrValues.push([parseInt(element['M']), parseInt(element['F'])]);
-      }
-      this.arrData.push(this.arrCab);
-      this.arrData.push(this.arrValues);
-      console.log(this.arrData)
-      google.charts.setOnLoadCallback(this.drawChart);
-    })
-  }
 
   drawChart = () => {
 
-    var data = google.visualization.arrayToDataTable(this.arrData);
+  const data = google.visualization.arrayToDataTable([
+    ['Task', 'Hours per Day'],
+    ['Work', 5],
+    ['Eat', 2],
+    ['Commute', 8],
+    ['Watch TV', 2],
+    ['Sleep', 7]
+  ]);
 
   const options = {
     title: 'My Daily Activities',
@@ -52,4 +29,9 @@ export class PieComponent implements OnInit {
 
   chart.draw(data, options);
 }
+
+  ngAfterViewInit() {
+    google.charts.load('current', { 'packages': ['corechart'] });
+    google.charts.setOnLoadCallback(this.drawChart);
+  }
 }
